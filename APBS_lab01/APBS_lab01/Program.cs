0586e11d19c3796032782f15d606ba123fcc4c62
s27891 <s27891@pjwstk.edu.pl>
2024-03-18 21:54:38 +0100

    Changes

--- APBS_lab01/APBS_lab01/Program.cs
+++ APBS_lab01/APBS_lab01/Program.cs
@@ -4,3 +4,16 @@ using APBS_lab01;
 
 Console.WriteLine("Hello, World!");
 Initial_project initialProject = new Initial_project(1, "GitHub");
+
+static float avg(int [] arr)
+{
+    int sum = 0;
+    for (int z = 0; z < arr.Length; z++)
+    {
+        sum += arr[z];
+    }
+
+    return (float)sum / arr.Length;
+}
+float average = avg(new int[]{1,2,3});
+Console.WriteLine(average);
