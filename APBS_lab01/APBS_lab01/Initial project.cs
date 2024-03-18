8a9c605c6478c75f9dd18fb020619885673fdb21
s27891 <s27891@pjwstk.edu.pl>
2024-03-18 20:19:38 +0100

    Added constructor of a Project class

--- APBS_lab01/APBS_lab01/Initial project.cs
+++ APBS_lab01/APBS_lab01/Initial project.cs
@@ -4,4 +4,10 @@ public class Initial_project
 {
     private int newField;
     private string nameOfProject;
+
+    public Initial_project(int newField, string nameOfProject)
+    {
+        this.newField = newField;
+        this.nameOfProject = nameOfProject;
+    }
 }
